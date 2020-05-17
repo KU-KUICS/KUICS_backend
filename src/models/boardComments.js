ebec4b0735c5f029314efb1420b4d3a15bc9a11c
BeLeap <ckdtj1012@naver.com>
2020-05-17 18:05:14 +0900

    boardComments 주석 추가

--- src/models/boardComments.js
+++ src/models/boardComments.js
@@ -1,17 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
     const boardComments = sequelize.define('boardComments', {
+        // Primary Key
         boardCommentsNo: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
         },
+        // 내용
         body: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        // 작성날짜
         writtenAt: {
             type: DataTypes.DATE,
         },
+        // 추천수
         recommendedTime: {
             type: DataTypes.INTEGER,
             defaultValue: 0,
