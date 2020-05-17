b13e80ed7253af7e050fff4ebb39d0bfa2939625
BeLeap <ckdtj1012@naver.com>
2020-05-17 18:02:59 +0900

    images 주석 추가

--- src/models/images.js
+++ src/models/images.js
@@ -1,10 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
     const Images = sequelize.define('images', {
+        // Primary Key
         imageNo: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
         },
+        // 이미지 파일 위치
         path: {
             type: DataTypes.STRING,
             allowNull: false,
