94ae758a16bad0484df762dc3f1f349a54bdaad5
Lanph3re <lanph3re@gmail.com>
2020-07-11 14:04:04 +0900

    passport 프로토

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -1,100 +1,15 @@
-<<<<<<< HEAD
-const jwt = require('jsonwebtoken');
-const joi = require('@hapi/joi');
-const crypto = require('crypto');
+const passport = require('passport');
 
-const { users } = require('../../../models');
-const { AUTH_KEY } = require('../../../config/config.json');
-
-const schema = joi.object({
-    username: joi.string().alphanum().min(3).max(30).required(),
-    password: joi
-        .string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
-        .required(),
+const getLogin = passport.authenticate('google', {});
+const getLoginCallback = passport.authenticate('google', {
+    failureRedirect: '/login',
+    successRedirect: '/',
 });
 
-/**
- * 로그인
- * @route POST /api/auth/login
- * @group Auth
- * @param { string } loginEntry.body.required - 로그인 입력
- * @returns { object } 200 - 유저 정보
- * @reutrns { Error } AUTH_INVALID_INPUT - AUTH_INVALID_INPUT
- */
-const postLogin = async (req, res, next) => {
-    try {
-        const { err, value } = await schema.validateAsync(req.body);
-
-        if (err) throw new Error('INVALID_PARAMETERS');
-
-        const { username, password } = value;
-
-        /* 해시 솔트 */
-        const hashPw = crypto
-            .createHash('sha-256')
-            .update(password)
-            .digest('hex');
-
-        const user = await users.findAll({
-            where: { username, password: hashPw },
-        });
-
-        if (!user) throw new Error('INVALID_PARAMETERS');
-
-        const token = jwt.sign(
-            {
-                role: user.level,
-            },
-            AUTH_KEY,
-            {
-                expiresIn: '1d',
-            },
-        );
-        res.cookie('token', token);
-        res.json({ token });
-        res.json(user);
-    } catch (err) {
-        next(new Error('INVALID_PARAMETERS'));
-=======
-const { users } = require('../../../models');
-
-const postAuth = async (req, res, next) => {
-    try {
-        const userInfo = req.body;
-        if (!userInfo) throw new Error('AUTH_NO_INPUT');
-
-        await users.create({
-            ...userInfo,
-            joinedAt: new Date(),
-            level: 0,
-            state: 0,
-        });
-
-        res.json({});
-    } catch (err) {
-        next(err);
->>>>>>> master
-    }
-};
-
-const postLogout = (req, res, next) => {};
-const postRegister = (req, res, next) => {};
-const postUnregister = (req, res, next) => {};
-const findId = (req, res, next) => {};
-const findPw = (req, res, next) => {};
-const changePw = (req, res, next) => {};
+const getLogout = (req, res, next) => {};
 
 module.exports = {
-<<<<<<< HEAD
-    postLogin,
-    postLogout,
-    postRegister,
-    postUnregister,
-    findId,
-    findPw,
-    changePw,
-=======
-    postAuth,
->>>>>>> master
+    getLogin,
+    getLoginCallback,
+    getLogout,
 };
--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,30 +1,10 @@
 const { Router } = require('express');
-<<<<<<< HEAD
-const {
-    postLogin,
-    postLogout,
-    postRegister,
-    postUnregister,
-    findId,
-    findPw,
-    changePw,
-} = require('./auth.ctrl');
+const { getLogin, getLoginCallback, getLogout } = require('./auth.ctrl');
 
 const router = Router();
 
-router.post('/login', postLogin);
-router.post('/logout', postLogout);
-router.post('/register', postRegister);
-router.post('/unregister', postUnregister);
-router.post('/find/:user_id', findId);
-router.post('/find/:user_pw', findPw);
-router.post('/change/password', changePw);
-=======
-const { postAuth } = require('./auth.ctrl');
-
-const router = Router();
-
-router.post('/', postAuth);
->>>>>>> master
+router.get('/login', getLogin);
+router.get('/login/callback', getLoginCallback);
+router.get('/logout', getLogout);
 
 module.exports = router;
