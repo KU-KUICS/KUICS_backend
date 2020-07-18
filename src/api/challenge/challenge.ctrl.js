4e1f8b89cd640986ea62000aca9a4f863c0a5b2b
Lanph3re <lanph3re@gmail.com>
2020-07-18 22:29:14 +0900

    플래그 제출 추가

--- src/api/challenge/challenge.ctrl.js
+++ src/api/challenge/challenge.ctrl.js
@@ -1,13 +1,21 @@
 const Joi = require('@hapi/joi');
 const { Op } = require('sequelize');
-const { users, challenges } = require('../../models');
+const { users, challenges, solvers } = require('../../models');
 
 /* 검증 스키마 */
 const numberSchema = Joi.number();
 
+const flagSubmitSchema = Joi.object({
+    challNo: numberSchema.required(),
+    flag: Joi.string()
+        .pattern(/^KUICS\{\w+\}$/) // 임시 플래그 포맷
+        .required(),
+});
+
 const isMember = async (email) => {
     const member = await users.findOne({
-        where: { email, level: { [Op.gte]: 1 } },
+        where: { email, state: 0, level: { [Op.gte]: 1 } },
+        attributes: ['userNo'],
     });
     return member;
 };
@@ -41,6 +49,7 @@ const getChallengesDesc = async (req, res, next) => {
                 exclude: ['challNo', 'createdAt', 'updatedAt', 'flag'],
             },
         });
+        if (!challenge) throw new Error('INVALID_PARAMETERS');
 
         res.json({ challenge });
     } catch (err) {
@@ -48,4 +57,38 @@ const getChallengesDesc = async (req, res, next) => {
     }
 };
 
-module.exports = { getChallenges, getChallengesDesc };
+const postSubmitFlag = async (req, res, next) => {
+    try {
+        const checkMember = await isMember(req.user.emails[0].value);
+        if (!checkMember) throw new Error('NOT_KUICS');
+
+        const { error, value } = flagSubmitSchema.validate(req.body);
+        if (error) throw new Error('INVALID_PARAMETERS');
+
+        const { challNo, flag } = value;
+        const challenge = await challenges.findOne({
+            where: { challNo },
+            attributes: ['challNo', 'flag', 'solvers'],
+        });
+        if (!challenge) throw new Error('INVALID_PARAMETERS');
+
+        if (flag !== challenge.flag) throw new Error('INVALID_PARAMETERS');
+
+        if (challenge.solvers === 0) {
+            challenge.userUserNo = checkMember.userNo;
+        }
+
+        challenge.solvers += 1;
+        await challenge.save();
+        await solvers.create({
+            userUserNo: checkMember.userNo,
+            challengeChallNo: challNo,
+        });
+
+        res.json({});
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = { getChallenges, getChallengesDesc, postSubmitFlag };
--- src/api/challenge/index.js
+++ src/api/challenge/index.js
@@ -1,14 +1,18 @@
 const { Router } = require('express');
-const { getChallenges, getChallengesDesc } = require('./challenge.ctrl');
+const {
+    getChallenges,
+    getChallengesDesc,
+    postSubmitFlag,
+} = require('./challenge.ctrl');
 
 const router = Router();
 
 /* TODO: isAuthenticated 추가 */
 router.get('/', getChallenges); // 문제 목록 불러오기
-router.get('/:challNo', getChallengesDesc); // 문제 목록 불러오기
+router.get('/:challNo', getChallengesDesc); // 문제 설명 불러오기
+router.post('/', postSubmitFlag); // 문제 플래그 인증
 
 // TODO:
-// 1. 플래그 인증
-// 2. 랭킹 출력
+// - 랭킹 출력
 
 module.exports = router;
