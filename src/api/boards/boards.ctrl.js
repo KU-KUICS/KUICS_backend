55592d9a48dedc03d6d2703bcf72609a31681919
lsjbh45 <lsjbh45@gmail.com>
2020-07-18 19:11:42 +0900

    api document 주석 추가, 수정

--- src/api/boards/boards.ctrl.js
+++ src/api/boards/boards.ctrl.js
@@ -128,6 +128,7 @@ const getBoardExcerpt = async (req, res, next) => {
  *  boardId에 해당하는 글 정보 가져오기
  *  @route GET /api/board/:boardId
  *  @group Board
+ *  @param {number} boardId.path.required - 글 번호
  *  @returns {Object, Array} 200 - 글 정보, 댓글 리스트
  *  @returns {Error} INVALID_PARAMETERS - invalid Parameters
  *  @returns {Error} NO_LOGIN - no login
@@ -275,6 +276,7 @@ const postBoard = async (req, res, next) => {
  *  boardId에 해당하는 글 수정하기
  *  @route PUT /api/board/:boardId
  *  @group Board
+ *  @param {number} boardId.path.required - 글 번호
  *  @param {boardScheme.model} boardScheme.body.required - 작성할 글 정보
  *  @returns {Object} 200 - 빈 객체
  *  @returns {Error} INVALID_PARAMETERS - invalid Parameters
@@ -325,6 +327,7 @@ const reviseBoard = async (req, res, next) => {
  *  boardId에 해당하는 글 삭제하기
  *  @route DELETE /api/board/:boardId
  *  @group Board
+ *  @param {number} boardId.path.required - 글 번호
  *  @returns {Object} 200 - 빈 객체
  *  @returns {Error} INVALID_PARAMETERS - invalid Parameters
  *  @returns {Error} NO_AUTH - unauthorized
@@ -359,6 +362,7 @@ const deleteBoard = async (req, res, next) => {
  *  boardId에 해당하는 글 추천하기
  *  @route POST /api/board/:boardId/recommend
  *  @group Board
+ *  @param {number} boardId.path.required - 글 번호
  *  @returns {Object} 200 - 빈 객체
  *  @returns {Error} INVALID_PARAMETERS - invalid Parameters
  *  @returns {Error} NO_AUTH - unauthorized
@@ -415,9 +419,15 @@ const recommendBoard = async (req, res, next) => {
 };
 
 /**
+ * @typedef commentScheme
+ * @property {string} body.required
+ */
+
+/**
  *  댓글 작성하기
  *  @route POST /api/board/:boardId/comment
  *  @group Board
+ *  @param {number} boardId.path.required - 글 번호
  *  @param {commentScheme.model} commentScheme.body.required - 작성할 댓글 정보
  *  @returns {Object} 200 - 빈 객체
  *  @returns {Error} INVALID_PARAMETERS - invalid Parameters
@@ -475,6 +485,8 @@ const postComment = async (req, res, next) => {
  *  댓글 수정하기
  *  @route PUT /api/board/:boardId/comment/:commentId
  *  @group Board
+ *  @param {number} boardId.path.required - 글 번호
+ *  @param {number} commentId.path.required - 댓글 번호
  *  @param {commentScheme.model} commentScheme.body.required - 작성할 댓글 정보
  *  @returns {Object} 200 - 빈 객체
  *  @returns {Error} INVALID_PARAMETERS - invalid Parameters
@@ -518,6 +530,8 @@ const reviseComment = async (req, res, next) => {
  *  댓글 삭제하기
  *  @route DELETE /api/board/:boardId/comment/:commentId
  *  @group Board
+ *  @param {number} boardId.path.required - 글 번호
+ *  @param {number} commentId.path.required - 댓글 번호
  *  @returns {Object} 200 - 빈 객체
  *  @returns {Error} INVALID_PARAMETERS - invalid parameters
  *  @returns {Error} NO_AUTH - unauthorized
@@ -558,6 +572,8 @@ const deleteComment = async (req, res, next) => {
  *  commentId에 해당하는 글 추천하기
  *  @route POST /api/board/:boardId/comment/:commentId/recommend
  *  @group Board
+ *  @param {number} boardId.path.required - 글 번호
+ *  @param {number} commentId.path.required - 댓글 번호
  *  @returns {Object} 200 - 빈 객체
  *  @returns {Error} INVALID_PARAMETERS - invalid parameters
  *  @returns {Error} NO_AUTH - unauthorized
