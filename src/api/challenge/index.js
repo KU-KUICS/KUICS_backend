ade22f5bc6f06b74c1d32099aefbc0f9dffe89df
Lanph3re <lanph3re@gmail.com>
2020-07-19 16:25:50 +0900

    challenge/index.js 개행 제거

--- src/api/challenge/index.js
+++ src/api/challenge/index.js
@@ -15,5 +15,4 @@ router.get('/', getChallenges); // 문제 목록 불러오기
 router.get('/:challNo', getChallengesDesc); // 문제 설명 불러오기
 router.post('/', postSubmitFlag); // 문제 플래그 인증
 
-
 module.exports = router;
