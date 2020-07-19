9db058bb920eaced4d93be6ffa85eebc9cf460ab
Lanph3re <lanph3re@gmail.com>
2020-07-19 14:49:06 +0900

    OAuth 리다이렉트 수정

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -6,15 +6,15 @@ const getLoginCallback = passport.authenticate('google', {
     scope: ['profile', 'email'],
     failureRedirect: '/api/auth/login/google/',
 
-    /* TODO: 나중에 front 루트로 수정 */
-    successRedirect: '/',
+    /* TODO: 나중에 product front루트/notice 로 수정 */
+    successRedirect: 'http://test.kuics.kro.kr/notice',
 });
 
 const getLogout = (req, res) => {
     req.logout();
 
-    /* TODO: 나중에 front 루트로 수정 */
-    res.redirect('/');
+    /* TODO: 나중에 product front 루트로 수정 */
+    res.redirect('http://test.kuics.kro.kr');
 };
 
 module.exports = {
