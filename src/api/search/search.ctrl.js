c6f0ef127d72616749e82fce371b5211bb79b0c8
zheedong <zheedong@gmailcom>
2020-07-30 16:51:37 +0900

    상세 검색 구현 중

--- src/api/search/search.ctrl.js
+++ src/api/search/search.ctrl.js
@@ -1,8 +1,10 @@
 const Joi = require('@hapi/joi');
 const { Op } = require('sequelize');
 
+// const { valid } = require('@hapi/joi');
 const { boards } = require('../../models');
 
+/*
 const searchKeyScheme = Joi.string().min(3).required();
 const searchTargetScheme = Joi.string()
     .valid('title', 'body', 'titleAndBody')
@@ -11,6 +13,17 @@ const searchTargetScheme = Joi.string()
 const searchInputScheme = Joi.object({
     key: searchKeyScheme,
     target: searchTargetScheme,
+});
+*/
+const searchKeyScheme = Joi.string();
+const searchDurationScheme = Joi.array[2]; // 구조 변경 필요 - from to 나눠서?
+
+const searchInputScheme = Joi.object({
+    title: searchKeyScheme,
+    body: searchKeyScheme,
+    duration: searchDurationScheme,
+    userName: searchKeyScheme,
+    tag: searchKeyScheme,
 });
 
 /**
@@ -22,6 +35,8 @@ const searchInputScheme = Joi.object({
  * @returns {Array} 200 - 검색 결과
  * @returns {Error} INVALID_PARAM - INVALID_PARAM
  */
+
+/*
 const getSearchResult = async (req, res, next) => {
     try {
         const { error, value } = searchInputScheme.validate(req.query);
@@ -60,6 +75,73 @@ const getSearchResult = async (req, res, next) => {
                     body: {
                         [Op.like]: searchBody,
                     },
+                },
+            },
+        });
+
+        res.json({ searchResult });
+    } catch (err) {
+        next(err);
+    }
+};
+*/
+
+const getSearchResult = async (req, res, next) => {
+    try {
+        const { error, value } = searchInputScheme.validate(req.query);
+        if (error) throw new Error('INVALID_PARAM');
+
+        const { title, body, duration, userName, tag } = value;
+
+        let searchTitle;
+        if (title) {
+            searchTitle = `%${title}%`;
+        } else {
+            searchTitle = `%%%`;
+        }
+
+        let searchBody;
+        if (body) {
+            searchBody = `%${body}%`;
+        } else {
+            searchBody = `%%%`;
+        }
+
+        /*
+        let searchDuration.array;
+        if (duration) {
+            searchDurationFrom = duration.array[0];
+        } else {
+            searchDurationFrom = 0;
+            searchDurationTo = MAX;
+        }
+        */
+
+        let searchUserName;
+        if (userName) {
+            searchUserName = `${userName}`;
+        } else {
+            searchUserName = `%%%`;
+        }
+
+        let searchTag;
+        if (searchTag) {
+            searchTag = `${tag}`;
+        } else {
+            searchTag = '%%%';
+        }
+
+        const searchResult = await boards.findAll({
+            where: {
+                [Op.or]: {
+                    title: {
+                        [Op.like]: searchTitle,
+                    },
+                    body: {
+                        [Op.like]: searchBody,
+                    },
+                    userName: searchUserName,
+                    tag: searchTag,
                 },
             },
         });
