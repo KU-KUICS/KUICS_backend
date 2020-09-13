fca81ff277340673eeb1bbb3a55e6ce853c59e19
zheedong <zheedong@gmailcom>
2020-09-13 20:04:39 +0900

    search 구현 변경
    AND로 검색
    TODO : bugfix 필요

--- src/api/search/search.ctrl.js
+++ src/api/search/search.ctrl.js
@@ -1,7 +1,7 @@
 const { Op } = require('sequelize');
 const { boards } = require('../../models');
 const { searchInputScheme } = require('../../lib/schemes');
-const users = require('../../models/users');
+const { users } = require('../../models');
 
 /**
  * 검색 결과를 가져옴
@@ -26,33 +26,26 @@ const getSearchResult = async (req, res, next) => {
             ? [new Date(duration[0]), new Date(duration[1])]
             : [new Date('2020'), new Date(Date.now())]; // 처음 글은 최소 2020년 1월에 작성
 
-        const searchUserName = userName ? `${userName}` : '%';
+        const searchUserID = userName
+            ? await users.findAll({
+                  attributes: ['userId'],
+                  where: {
+                      userName,
+                  },
+              })
+            : '%';
 
         const searchTag = tag ? `${tag}` : '%';
 
-        const searchUserID = await users.findAll({
-            attributes: ['userId'],
-            where: {
-                userName: searchUserName,
-            },
-        });
-
         const searchResult = await boards.findAll({
             where: {
-                [Op.or]: [
-                    { title: { [Op.like]: searchTitle } },
-                    { body: { [Op.like]: searchBody } },
-                    {
-                        createdAt: {
-                            [Op.between]: [
-                                searchDuration[0],
-                                searchDuration[1],
-                            ],
-                        },
-                    },
-                    { userID: searchUserID }, // user와 board Join 구현 필요
-                    { tag: searchTag }, // board에 아직 기능 추가 안 됨
-                ],
+                title: { [Op.like]: searchTitle },
+                body: { [Op.like]: searchBody },
+                createdAt: {
+                    [Op.between]: [searchDuration[0], searchDuration[1]],
+                },
+                userID: searchUserID, // TODO: user와 board Join 구현 필요
+                //  tag: searchTag , // ISSUE: board에 아직 기능 추가 안 됨
             },
         });
 
